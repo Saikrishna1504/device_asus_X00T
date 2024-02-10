233175372fa7d3f5d315823baca9520a3b3fb49e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-10 16:01:22 +0530

    X00T: Declare notification LED capabilities
    Change-Id: I89dab65991abcce2bcad52d65bdeb31fa637f334
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -291,6 +291,9 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-voltage
+
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayX00T \
     CellBroadcastReceiverOverlayX00T \
